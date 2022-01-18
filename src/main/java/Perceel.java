08c203faf29a9bb2a197c0e86eb48f7fb914f14d
IvHaaften <inge@van-haaften.nl>
2022-01-18 15:25:27 +0100

    Add perceelTypes

--- src/main/java/Perceel.java
+++ src/main/java/Perceel.java
@@ -1,4 +1,13 @@
 package src.main.java;
 
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 public class Perceel {
+    UUID id;
+    int oppervlakte;
+    List<Map<Double, Double>> begrenzing;
+    boolean beperking;
+    Aardbewoner eigenaar;
 }
---
+++ src/main/java/PerceelTypes/LangbouwPerceel.java
@@ -0,0 +1,7 @@
+package src.main.java.PerceelTypes;
+
+import src.main.java.Perceel;
+
+public class LangbouwPerceel extends Perceel {
+
+}
