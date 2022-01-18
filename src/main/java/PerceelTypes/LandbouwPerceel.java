f830d6f976bff5361fb6fa1ae8a534b99bde511b
IvHaaften <inge@van-haaften.nl>
2022-01-18 15:26:35 +0100

    fix LandbouwPerceel

---
+++ src/main/java/PerceelTypes/LandbouwPerceel.java
@@ -0,0 +1,8 @@
+package src.main.java.PerceelTypes;
+
+import src.main.java.Perceel;
+
+public class LandbouwPerceel extends Perceel {
+    Gewas gewas;
+    int jaarOpbrengst;
+}
--- src/main/java/PerceelTypes/LangbouwPerceel.java
+++
@@ -1,7 +0,0 @@
-package src.main.java.PerceelTypes;
-
-import src.main.java.Perceel;
-
-public class LangbouwPerceel extends Perceel {
-
-}
