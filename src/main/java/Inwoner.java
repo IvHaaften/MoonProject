39505862d061eacc2552a6a60a5b7e939694a03f
Robert oude Lohuis <97960031+Robert-o-L@users.noreply.github.com>
2022-01-18 16:00:49 +0100

    getters/setters

--- src/main/java/Inwoner.java
+++ src/main/java/Inwoner.java
@@ -1,5 +1,15 @@
 package src.main.java;
 
+import java.util.List;
+
 public class Inwoner {
-    Perceel[] bezetenPercelen;
+    private List<Perceel> Percelen;
+
+	public List<Perceel> getPercelen() {
+		return Percelen;
+	}
+
+	public void setPercelen(List<Perceel> percelen) {
+		Percelen = percelen;
+	}
 }
--- src/main/java/Transactie.java
+++ src/main/java/Transactie.java
@@ -3,8 +3,33 @@ package src.main.java;
 import java.time.LocalDate;
 
 public class Transactie {
-    LocalDate datum;
-    Inwoner verkoper;
-    Inwoner aankoper;
-    Perceel perceel;
+    private LocalDate datum;
+    private Inwoner verkoper;
+    private Inwoner aankoper;
+    private Perceel perceel;
+    
+	public LocalDate getDatum() {
+		return datum;
+	}
+	public void setDatum(LocalDate datum) {
+		this.datum = datum;
+	}
+	public Inwoner getVerkoper() {
+		return verkoper;
+	}
+	public void setVerkoper(Inwoner verkoper) {
+		this.verkoper = verkoper;
+	}
+	public Inwoner getAankoper() {
+		return aankoper;
+	}
+	public void setAankoper(Inwoner aankoper) {
+		this.aankoper = aankoper;
+	}
+	public Perceel getPerceel() {
+		return perceel;
+	}
+	public void setPerceel(Perceel perceel) {
+		this.perceel = perceel;
+	}
 }
