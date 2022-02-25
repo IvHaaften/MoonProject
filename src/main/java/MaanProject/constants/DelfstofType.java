487ef202dd4a90f6375ea19835e83dc4573ccbe7
IvHaaften <inge@van-haaften.nl>
2022-02-25 14:38:37 +0100

    Delfstof heeft nu boolean voor radioactiviteit

--- src/main/java/MaanProject/constants/DelfstofType.java
+++ src/main/java/MaanProject/constants/DelfstofType.java
@@ -1,3 +1,14 @@
 package MaanProject.constants;
 
-public enum DelfstofType {HELIUM3, IRIDIUM, REGOLIET, GOUD, PALLADIUM, PLATINA}
+public enum DelfstofType {
+    HELIUM3(false), IRIDIUM(true), REGOLIET(false), GOUD(false), PALLADIUM(false), PLATINA(false);
+    private final boolean radioactief;
+
+    public boolean isRadioactief() {
+        return radioactief;
+    }
+
+    DelfstofType(boolean radioactief) {
+        this.radioactief = radioactief;
+    }
+}
